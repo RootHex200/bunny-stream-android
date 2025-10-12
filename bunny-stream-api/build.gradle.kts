56a23978af8beb4321030c30f1ea02902c9d7b1c
Roothex200 <sabiturrahman60@gmail.com>
2025-10-12 12:57:09 +0600

    remove MinifyEnabled false

--- bunny-stream-api/build.gradle.kts
+++ bunny-stream-api/build.gradle.kts
@@ -48,11 +48,11 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+//            isMinifyEnabled = true
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
         }
 
         getByName("debug") {
--- bunny-stream-player/build.gradle.kts
+++ bunny-stream-player/build.gradle.kts
@@ -34,11 +34,11 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+//            isMinifyEnabled = true
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
         }
 
         getByName("debug") {
